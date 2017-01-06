3e61fba385b6086c5516127ed68a0e5e657fbc85
Naim Sharif <naimsharif@Naims-MacBook-Air.local>
2017-01-05 21:58:25 -0600

    Added mad libs program, will update with more efficient

---
+++ MadLibs/mad.py
@@ -0,0 +1,12 @@
+print("Welcome to madlibs, lets start \n")
+
+
+noun1 = input("Enter a noun \n")
+verb1 = input("Enter a verb \n")
+adj1 = input("Enter a adjective \n")
+
+list = [noun1 , verb1, adj1]
+
+Story = ("I once met a " + noun1 + ", it was trying to " + verb1 + " so hard that " + noun1 + " was know as the " + adj1 + " " + noun1 + "\n")
+
+input(Story)
